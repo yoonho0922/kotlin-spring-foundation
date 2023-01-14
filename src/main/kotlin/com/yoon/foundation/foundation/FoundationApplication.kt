fc4dcc511f21881cad5fc93db315d8808d58a21c
Yoonho Ahn <yoonho5684@gmail.com>
2023-01-14 15:37:25 +0900

    init

---
+++ src/main/kotlin/com/yoon/foundation/foundation/FoundationApplication.kt
@@ -0,0 +1,11 @@
+package com.yoon.foundation.foundation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoundationApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoundationApplication>(*args)
+}
